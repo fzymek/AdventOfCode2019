280890690e72c8941c47ae13c17a1903e9be8649
Filip Zymek <zymfilip@amazon.com>
2019-12-01 22:48:26 +0000

    Initial commit

---
+++ AdventOfCode2019/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  AdventOfCode2019
+//
+//  Created by Zymek, Filip on 2019-12-01.
+//  Copyright © 2019 Filip Zymek. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
